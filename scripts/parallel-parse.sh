e595610e8b8e57139070d4e12e1ab03c4e4fc7fb
Akiva Miura <akiva.miura@gmail.com>
2016-05-17 11:13:45 +0900

    Added option for tmpdir

--- scripts/parallel-parse.sh
+++ scripts/parallel-parse.sh
@@ -7,6 +7,7 @@ if [ ${#ARGS[@]} -ne 3 ]; then
   echo "usage: lang infile outfile" > /dev/stderr
   echo ""
   echo "--splitsize={int}"
+  echo "--tmp={dirname}"
   exit 1
 fi
 
@@ -42,22 +43,22 @@ wait_file() {
 check_parse() {
   local INFILE=$1
   local BASE=$(basename $INFILE .toparse)
-  if [[ ! -f ${WORKDIR}/tmp/DONE.parse.${BASE} ]]; then
-    if [[ ! -f ${WORKDIR}/tmp/START.parse.${BASE} ]]; then
-      show_exec echo ${WORKID} \> ${WORKDIR}/tmp/START.parse.${BASE}
+  if [[ ! -f ${TMPDIR}/DONE.parse.${BASE} ]]; then
+    if [[ ! -f ${TMPDIR}/START.parse.${BASE} ]]; then
+      show_exec echo ${WORKID} \> ${TMPDIR}/START.parse.${BASE}
       show_exec sleep 1
-      local TMPID=$(cat ${WORKDIR}/tmp/START.parse.${BASE})
+      local TMPID=$(cat ${TMPDIR}/START.parse.${BASE})
       if [[ "${TMPID}" == "${WORKID}" ]]; then
-        show_exec ${CKYLARK}/src/bin/ckylark --model ${MODEL} --input ${INFILE} --output ${WORKDIR}/tmp/${BASE}.parsed --add-root-tag
-        show_exec echo ${WORKID} \> ${WORKDIR}/tmp/DONE.parse.${BASE}
+        show_exec ${CKYLARK}/src/bin/ckylark --model ${MODEL} --input ${INFILE} --output ${TMPDIR}/${BASE}.parsed --add-root-tag
+        show_exec echo ${WORKID} \> ${TMPDIR}/DONE.parse.${BASE}
       else
-        echo "File exists: ${WORKDIR}/tmp/START.parse.${BASE}" $(cat ${WORKDIR}/tmp/START.parse.${BASE})
+        echo "File exists: ${TMPDIR}/START.parse.${BASE}" $(cat ${TMPDIR}/START.parse.${BASE})
       fi
     else
-      echo "File exists: ${WORKDIR}/tmp/START.parse.${BASE}" $(cat ${WORKDIR}/tmp/START.parse.${BASE})
+      echo "File exists: ${TMPDIR}/START.parse.${BASE}" $(cat ${TMPDIR}/START.parse.${BASE})
     fi
   else
-    echo "File exists: ${WORKDIR}/tmp/DONE.parse.${BASE}" $(cat ${WORKDIR}/tmp/DONE.parse.${BASE})
+    echo "File exists: ${TMPDIR}/DONE.parse.${BASE}" $(cat ${TMPDIR}/DONE.parse.${BASE})
   fi
 }
 
@@ -68,48 +69,55 @@ export WORKDIR
 export WORKID
 export MODEL
 export CKYLARK
+export TMPDIR
 
 WORKDIR=$(dirname $OUTFILE)
 BASE=$(basename $INFILE)
 WORKID="${HOST}:$$"
+
+TMPDIR=./tmp
+if [ "${opt_tmp}" ]; then
+  TMPDIR="${opt_tmp}"
+fi
+
 if [ ! -f ${WORKDIR}/DONE.parse.${BASE} ]; then
-  if [[ ! -d ${WORKDIR}/tmp ]]; then
-    show_exec mkdir -p ${WORKDIR}/tmp
+  if [[ ! -d ${TMPDIR} ]]; then
+    show_exec mkdir -p ${TMPDIR}
   fi
-  if [ ! -f ${WORKDIR}/tmp/DONE.split.${BASE} ]; then
-    if [ -f ${WORKDIR}/tmp/START.split.${BASE} ]; then
-      wait_file ${WORKDIR}/tmp/DONE.split.${BASE}
+  if [ ! -f ${TMPDIR}/DONE.split.${BASE} ]; then
+    if [ -f ${TMPDIR}/START.split.${BASE} ]; then
+      wait_file ${TMPDIR}/DONE.split.${BASE}
     else
-      show_exec echo ${WORKID} \> ${WORKDIR}/tmp/START.split.${BASE}
+      show_exec echo ${WORKID} \> ${TMPDIR}/START.split.${BASE}
       show_exec sleep 1
-      TMPID=$(cat ${WORKDIR}/tmp/START.split.${BASE})
+      TMPID=$(cat ${TMPDIR}/START.split.${BASE})
       if [[ "${TMPID}" == "${WORKID}" ]]; then
-        show_exec split -a 4 -l ${SPLITSIZE} ${INFILE} ${WORKDIR}/tmp/${BASE}.
-        for file in ${WORKDIR}/tmp/${BASE}.*; do
+        show_exec split -a 4 -l ${SPLITSIZE} ${INFILE} ${TMPDIR}/${BASE}.
+        for file in ${TMPDIR}/${BASE}.*; do
           show_exec mv ${file} ${file}.toparse
         done
-        show_exec echo ${WORKID} \> ${WORKDIR}/tmp/DONE.split.${BASE}
+        show_exec echo ${WORKID} \> ${TMPDIR}/DONE.split.${BASE}
       fi
     fi
   fi
-#  show_exec ls ${WORKDIR}/tmp/${BASE}.\*.toparse \| parallel -j ${THREADS} check_parse {}
-  show_exec find ${WORKDIR}/tmp \| grep -E '".toparse$"' \| parallel -j ${THREADS} check_parse {}
-  if [ -f ${WORKDIR}/tmp/START.merge.${BASE} ]; then
-    echo "File exists: ${WORKDIR}/tmp/START.merge.${BASE}" $(cat ${WORKDIR}/tmp/START.merge.${BASE})
-    wait_file ${WORKDIR}/tmp/DONE.merge.${BASE}
+#  show_exec ls ${TMPDIR}/${BASE}.\*.toparse \| parallel -j ${THREADS} check_parse {}
+  show_exec find ${TMPDIR} \| grep -E '".toparse$"' \| parallel -j ${THREADS} check_parse {}
+  if [ -f ${TMPDIR}/START.merge.${BASE} ]; then
+    echo "File exists: ${TMPDIR}/START.merge.${BASE}" $(cat ${TMPDIR}/START.merge.${BASE})
+    wait_file ${TMPDIR}/DONE.merge.${BASE}
   else
-    show_exec echo ${WORKID} \> ${WORKDIR}/tmp/START.merge.${BASE}
+    show_exec echo ${WORKID} \> ${TMPDIR}/START.merge.${BASE}
     show_exec sleep 1
-    TMPID=$(cat ${WORKDIR}/tmp/START.merge.${BASE})
+    TMPID=$(cat ${TMPDIR}/START.merge.${BASE})
     if [[ "${TMPID}" == "${WORKID}" ]]; then
-      for file in ${WORKDIR}/tmp/${BASE}.*.toparse; do
-        wait_file ${WORKDIR}/tmp/DONE.parse.$(basename $file .toparse)
+      for file in ${TMPDIR}/${BASE}.*.toparse; do
+        wait_file ${TMPDIR}/DONE.parse.$(basename $file .toparse)
       done
-      show_exec cat ${WORKDIR}/tmp/${BASE}.\*.parsed \| pv -Wl \> ${OUTFILE}
-      show_exec echo ${WORKID} \> ${WORKDIR}/tmp/DONE.merge.${BASE}
+      show_exec cat ${TMPDIR}/${BASE}.\*.parsed \| pv -Wl \> ${OUTFILE}
+      show_exec echo ${WORKID} \> ${TMPDIR}/DONE.merge.${BASE}
     fi
   fi
-  #show_exec rm -rf ${WORKDIR}/tmp
+  #show_exec rm -rf ${TMPDIR}
   show_exec echo ${WORKID} \> ${WORKDIR}/DONE.parse.${BASE}
 fi
 
