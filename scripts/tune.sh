48d263fd9b9e155339df54035010b51928f81e9b
Akiva Miura <akiva.miura@gmail.com>
2018-01-26 14:41:23 +0900

    Added option to specify alternative language model and task identifier for tuning

--- scripts/tune.sh
+++ scripts/tune.sh
@@ -5,14 +5,16 @@ source "${dir}/common.sh"
 
 TUNE_NBEST=200
 EVAL=bleu
+MAX_ITERS=20
 
 usage()
 {
-  echo "usage: $0 mt_method corpus1 corpus2 inifile task_dir"
+  echo "usage: $0 mt_method corpus1 corpus2 inifile task_dir [tune_name [alter_lm]]"
   echo ""
   echo "options:"
   echo "  --threads={integer}"
   echo "  --eval={string}"
+  echo "  --max_iters={integer}"
 }
 
 if [ ${#ARGS[@]} -lt 5 ]
@@ -21,21 +23,31 @@ then
   exit 1
 fi
 
-if [ ${opt_threads} ]; then
-  THREADS=${opt_threads}
-fi
-
-mert_out="mert-work"
-if [ "${opt_eval}" ]; then
-  EVAL=${opt_eval}
-  mert_out="mert-work-${opt_eval}"
-fi
-
 mt_method=${ARGS[0]}
 src1=$(abspath ${ARGS[1]})
 src2=$(abspath ${ARGS[2]})
 inifile=$(abspath ${ARGS[3]})
 task=${ARGS[4]}
+tune_name=${ARGS[5]}
+alter_lm=${ARGS[6]}
+
+if [ ${opt_threads} ]; then
+  THREADS=${opt_threads}
+fi
+
+if [ ${opt_max_iters} ]; then
+    MAX_ITERS=${opt_max_iters}
+fi
+
+mert_out="mert-work"
+if [ "${tune_name}" ]; then
+    mert_out="${mert_out}-${tune_name}"
+fi
+if [ "${opt_eval}" ]; then
+  EVAL=${opt_eval}
+  #mert_out="mert-work-${opt_eval}"
+  mert_out="${mert_out}-${EVAL}"
+fi
 
 workdir="${task}/working"
 show_exec mkdir -p ${workdir}
@@ -48,7 +60,7 @@ case "${mt_method}" in
     ;;
   hiero|scfg)
     options=""
-    trg_factors=$(grep -1 trg_factors $inifile | tail -n1)
+    trg_factors=$(grep -1 trg_factors $inifile | tail -n 1)
     if [ "${trg_factors}" ]; then
       options="-trg-factors ${trg_factors}"
       if [ ${trg_factors} -gt 1 ]; then
@@ -56,9 +68,19 @@ case "${mt_method}" in
         if [ "${opt_eval}" ]; then
           EVAL="${opt_eval}:factor=0"
         fi
+        if [ ! "${opt_max_iters}" ]; then
+            MAX_ITERS=30
+        fi
       fi
     fi
-    show_exec $TRAVATAR/script/mert/mert-travatar.pl -travatar-config ${inifile} -nbest ${TUNE_NBEST} -src ${src1} -ref ${src2} -travatar-dir ${TRAVATAR} -working-dir ${workdir}/${mert_out} -in-format word -threads ${THREADS} -eval ${EVAL} ${options} -resume
+    if [ "${alter_lm}" ]; then
+        abs_lm=$(abspath ${alter_lm})
+        show_exec cat ${inifile} \| sed -e "'/\\[lm_file\\]/ { n; s#.*#${abs_lm}# }'" \> ${workdir}/travatar-${tune_name}.ini
+        inifile=${workdir}/travatar-${tune_name}.ini
+
+    fi
+    #show_exec $TRAVATAR/script/mert/mert-travatar.pl -travatar-config ${inifile} -nbest ${TUNE_NBEST} -src ${src1} -ref ${src2} -travatar-dir ${TRAVATAR} -working-dir ${workdir}/${mert_out} -in-format word -threads ${THREADS} -eval ${EVAL} ${options} -resume
+    show_exec $TRAVATAR/script/mert/mert-travatar.pl -travatar-config ${inifile} -nbest ${TUNE_NBEST} -src ${src1} -ref ${src2} -travatar-dir ${TRAVATAR} -working-dir ${workdir}/${mert_out} -in-format word -threads ${THREADS} -eval ${EVAL} -max-iters ${MAX_ITERS} ${options} -resume
     ;;
   t2s)
     show_exec $TRAVATAR/script/mert/mert-travatar.pl -travatar-config ${inifile} -nbest ${TUNE_NBEST} -src ${src1} -ref ${src2} -travatar-dir ${TRAVATAR} -working-dir ${workdir}/${mert_out} -in-format penn -threads ${THREADS} -eval ${EVAL} -resume
@@ -81,5 +103,10 @@ else
   show_exec rm ${workdir}/${mert_out}/*.nbest
   show_exec rm ${workdir}/${mert_out}/*.stats
   show_exec rm ${workdir}/${mert_out}/*.uniq
+fi
+
+if [ "${tune_name}" ]; then
+    show_exec mkdir -p ${task}/${tune_name}
+    cp ${workdir}/${mert_out}/travatar.ini ${task}/${tune_name}/
 fi
 
